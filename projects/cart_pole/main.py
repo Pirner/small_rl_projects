59226f27fe151d97024227ee2c89d324520b5860
Stephan.Pirner <stephan.pirner93@gmail.com>
2022-05-07 14:50:49 +0200

    upload some stuff

---
+++ projects/cart_pole/main.py
@@ -0,0 +1,9 @@
+from projects.cart_pole.learning import CartpoleAI
+
+
+if __name__ == "__main__":
+    # agent = DQNAgent()
+    # agent.run()
+    # agent.test()
+    agent = CartpoleAI()
+    agent.run()
