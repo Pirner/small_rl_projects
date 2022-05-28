394f66053802facbfd0138e91c6998ca5885f032
Stephan.Pirner <stephan.pirner93@gmail.com>
2022-05-28 17:48:28 +0200

    fixup! fixup for target model for balance

---
+++ projects/beginner/lunar_lander/dqn_own_approach/run_trained_model.py
@@ -0,0 +1,35 @@
+import gym
+from tensorflow.keras.models import load_model
+import time
+
+from agent import predict_q_values
+from agent import select_best_action
+from loss import masked_huber_loss
+
+
+def main():
+    model_path = r'D:\rl_stuff\lunar_lander\last_models\model_700.h5'
+    model = load_model(
+        model_path,
+        custom_objects={'masked_huber_loss': masked_huber_loss(0.0, 1.0)},
+    )
+
+    for i in range(20):
+
+        env = gym.make("LunarLander-v2")
+        state = env.reset()
+
+        while True:
+            env.render()
+            q_values = predict_q_values(model=model, state=state)
+            best_action = select_best_action(q_values)
+            new_state, reward, done, info = env.step(action=best_action)
+            print(best_action)
+            time.sleep(0.01)
+            state = new_state
+            if done:
+                break
+
+
+if __name__ == '__main__':
+    main()
