3feba009ea9c0614e4d839157a104516ef6c5873
Stephan.Pirner <stephan.pirner93@gmail.com>
2022-05-29 18:33:57 +0200

    build first working approach with fraction (faster conversion)

---
+++ projects/beginner/lunar_lander/scripting/DTO.py
@@ -0,0 +1,7 @@
+class StateTransition:
+    def __init__(self, old_state, action, reward, new_state, done):
+        self.old_state = old_state
+        self.action = action
+        self.reward = reward
+        self.new_state = new_state
+        self.done = done
--- projects/beginner/lunar_lander/scripting/lld_trainer.py
+++ projects/beginner/lunar_lander/scripting/lld_trainer.py
@@ -1,11 +1,19 @@
+import shutil
+import uuid
 from typing import Tuple
+
+from tensorflow.python.keras.models import load_model
 from tqdm import tqdm
 import numpy as np
+import random
 
 import tensorflow as tf
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Dense
 import gym
+
+from DTO import StateTransition
+from replay import ReplayBuffer
 
 
 class LunarLanderTrainer:
@@ -20,6 +28,19 @@ class LunarLanderTrainer:
         self._max_steps = 1000
         self._max_episodes = 10000
         self._step_count = 0
+        self._replay_buffer_size = 200000
+        self._target_network_replace_frequency_steps = 1000
+        self._step_count = 0
+        self._replay_buffer = ReplayBuffer(self._replay_buffer_size)
+        self._training_start = 256
+        self._train_every_x_steps = 4
+        self._training_batch_size = 128
+        self._discount_factor = 0.99
+        self._epsilon_decay_factor_per_episode = 0.995
+        self._starting_epsilon = 1.0
+        self._epsilon = self._starting_epsilon
+        self._model_backup_frequency_episodes = 25
+        self._minimum_epsilon = 0.01
 
         self._model = LunarLanderTrainer.create_model(
             lr=self._lr,
@@ -48,23 +69,132 @@ class LunarLanderTrainer:
         """
         # reset values
         self._step_count = 0
+        self._discount_factor = 0.99
+        self._epsilon = self._starting_epsilon
 
-        for episode in tqdm(self._max_episodes, total=self._max_episodes):
-            episode_reward = 0
-            state = self._env.reset()
-            fraction_finished = 0.0
-            state = np.append(state, fraction_finished)
-
+        for episode in tqdm(range(self._max_episodes), total=self._max_episodes):
             self._run_episode()
 
-    def _run_episode(selfp):
+            if episode != 0 and episode % self._model_backup_frequency_episodes == 0:
+                backup_file = f"model_{episode}.h5"
+                print(f"Backing up model to {backup_file}")
+                self._model.save(backup_file)
+
+            self._epsilon *= self._epsilon_decay_factor_per_episode
+            self._epsilon = max(self._minimum_epsilon, self._epsilon)
+
+    def _run_episode(self):
         """
         run an episode in the environment
         :return:
         """
+        state = self._env.reset()
+        episode_reward = 0
+
+        fraction_finished = 0.0
+        state = np.append(state, fraction_finished)
+
         for step in range(1, self._max_steps + 1):
             self._step_count += 1
-            qs_model = LunarLanderTrainer.predict_q_values(self._model)
+            q_values_model = LunarLanderTrainer.predict_q_values(model=self._model, state=state)
+            action = LunarLanderTrainer.select_action_epsilon_greedy(q_values=q_values_model, epsilon=self._epsilon)
+
+            new_state, reward, done, info = self._env.step(action)
+            fraction_finished = (step + 1) / self._max_steps
+            new_state = np.append(new_state, fraction_finished)
+
+            episode_reward += reward
+
+            if step == self._max_steps:
+                print('Episode finished fish maximum number of steps')
+                done = True
+
+            state_transition = StateTransition(
+                old_state=state,
+                action=action,
+                reward=reward,
+                new_state=new_state,
+                done=done,
+            )
+            self._replay_buffer.add(transition=state_transition)
+            state = new_state
+
+            if self._step_count % self._target_network_replace_frequency_steps == 0:
+                print('Upadting target model')
+                self.copy_model()
+
+            if self._replay_buffer.length() >= self._training_start and self._step_count % self._train_every_x_steps == 0:
+                batch = self._replay_buffer.get_batch(batch_size=self._training_batch_size)
+                targets = self._calculate_target_values(
+                    state_transitions=batch,
+                    discount_factor=self._discount_factor
+                )
+                states = np.array([state_transition.old_state for state_transition in batch])
+                LunarLanderTrainer.train_model(model=self._model, states=states, targets=targets)
+
+            if done:
+                break
+
+    def _calculate_target_values(self, state_transitions, discount_factor):
+        """
+
+        :param state_transitions:
+        :param discount_factor:
+        :return:
+        """
+        states = []
+        new_states = []
+        for transition in state_transitions:
+            states.append(transition.old_state)
+            new_states.append(transition.new_state)
+
+        states = np.array(states)
+        new_states = np.array(new_states)
+        q_val_new_states = LunarLanderTrainer.get_multiple_q_values(model=self._model, states=new_states)
+        q_val_new_states_tg_model = LunarLanderTrainer.get_multiple_q_values(
+            model=self._target_model,
+            states=new_states
+        )
+        targets = []
+
+        for index, state_transition in enumerate(state_transitions):
+            best_action = LunarLanderTrainer.select_action_epsilon_greedy(
+                q_values=q_val_new_states[index],
+                epsilon=self._epsilon,
+            )
+            best_action_next_state_q_val = q_val_new_states_tg_model[index][best_action]
+
+            if state_transition.done:
+                target_value = state_transition.reward
+            else:
+                target_value = state_transition.reward + discount_factor * best_action_next_state_q_val
+
+            target_vector = [0, 0, 0, 0]
+            target_vector[state_transition.action] = target_value
+            targets.append(target_vector)
+
+        return np.array(targets)
+
+    def copy_model(self) -> None:
+        backup_file = 'backup_' + str(uuid.uuid4())
+        self._model.save(backup_file)
+        # self._target_model = load_model(
+        # backup_file, custom_objects={'masked_huber_loss': masked_huber_loss(0.0, 1.0)})
+        self._target_model = load_model(backup_file)
+        shutil.rmtree(backup_file)
+
+    @staticmethod
+    def train_model(model, states, targets):
+        model.fit(states, targets, epochs=1, batch_size=len(targets), verbose=0)
+
+    @staticmethod
+    def select_action_epsilon_greedy(q_values, epsilon):
+        random_value = random.uniform(0, 1)
+        if random_value < epsilon:
+            return random.randint(0, len(q_values) - 1)
+
+        else:
+            return np.argmax(q_values)
 
     @staticmethod
     def predict_q_values(model, state):
@@ -76,6 +206,10 @@ class LunarLanderTrainer:
         """
         x_in = state[np.newaxis, ...]
         return model.predict(x_in)[0]
+
+    @staticmethod
+    def get_multiple_q_values(model, states):
+        return model.predict(states)
 
     @staticmethod
     def create_model(lr: float, reg_factor: float, output_shape: Tuple[int], input_shape: Tuple[int]):
@@ -103,3 +237,4 @@ class LunarLanderTrainer:
 
 if __name__ == '__main__':
     trainer = LunarLanderTrainer()
+    trainer.learn_lunar()
